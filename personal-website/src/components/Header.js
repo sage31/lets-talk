78c84e97e720a382268b80aa616eaf6fe72141fa
Stevie Dean <stephenwdean@gmail.com>
2024-02-02 22:09:39 -0800

    Base

---
+++ personal-website/src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div className="inline">
+      <img
+        src="/images/me.jpg"
+        alt="Stevie Dean"
+        className="rounded-full"
+        width="12%"
+      />
+      <h1>Stevie Dean</h1>
+    </div>
+  );
+}
