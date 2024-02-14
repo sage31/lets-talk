92d994bd696ab3a6ab19c149c0f29a808ddf306d
Stevie Dean <stephenwdean@gmail.com>
2024-02-14 15:45:59 -0800

    font changes for different screen sizes

--- personal-website/src/components/Header.js
+++ personal-website/src/components/Header.js
@@ -7,11 +7,11 @@ export default function Header() {
           alt="Stevie Dean"
           className="inline w-[23%]  laptop:w-[23%] left-[0%] laptop:pr-4 aspect-auto"
         />
-        <div className="self-center ml-3 mt-3 laptop:ml-0 laptop:mt-0 laptop:pt-10">
+        <div className="self-center ml-1 mt-3 laptop:ml-0 laptop:mt-0 laptop:pt-10">
           <div>
-            <span className="inline flex max-se:text-sm text-lg laptop:text-3xl lg:text-4xl font-bold">
+            <span className="inline flex max-se:text-xs text-lg laptop:text-3xl lg:text-4xl font-light font-bold">
               Stevie Dean |&nbsp;
-              <div className="text-xs font-semibold laptop:text-lg lg:text-2xl flex items-center mt-1 laptop:items-end">
+              <div className="text-xs font-light font-semibold laptop:text-lg lg:text-2xl flex items-center mt-1 laptop:items-end">
                 <a
                   href="https://docs.google.com/document/d/1VTOeQvjoh9H5I34Ci9Hzr04WyFetJ8yR/edit?usp=sharing&ouid=104446278302311002073&rtpof=true&sd=true"
                   target="_blank"
@@ -40,7 +40,7 @@ export default function Header() {
                 </a>
               </div>
             </span>
-            <h2 className="font-bold text-xs laptop:text-xl lg:text-xl lg:mt-2">
+            <h2 className="font-light text-xs laptop:text-xl lg:text-xl lg:mt-2">
               Santa Clara CS '25 • Aspiring Software Engineer
             </h2>
           </div>
