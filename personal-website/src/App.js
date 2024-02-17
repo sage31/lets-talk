a32234e8aa1c193e6016025e0c5f48d6a4557016
Stevie Dean <stephenwdean@gmail.com>
2024-02-17 10:23:22 -0800

    experimenting with styles for mobile

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App overflow-y-hidden">
+    <div className="App h-full overflow-y-hidden">
       <Header />
       <ChatWindow />
     </div>
