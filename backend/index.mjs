25c499a9b50def4fccf382b8b5f7863b4492abfa
Stevie Dean <stephenwdean@gmail.com>
2025-04-18 18:46:45 -0700

    backend error handling

--- backend/index.mjs
+++ backend/index.mjs
@@ -6,54 +6,80 @@ const bigInvalidBytes = Buffer.alloc(1024 * 50, invalidUtfChar); // 50KB, helps 
 
 export const handler = awslambda.streamifyResponse(
   async (event, responseStream, _context) => {
-    let message = JSON.parse(event.body).message;
-    let threadId = event.headers["thread-id"];
-    if (!threadId) {
-      const thread = await openai.beta.threads.create({
-        messages: [message],
+    try {
+      let message = JSON.parse(event.body).message;
+      let threadId = event.headers["thread-id"];
+      if (!threadId) {
+        const thread = await openai.beta.threads.create({
+          messages: [message],
+        });
+        threadId = thread.id;
+      }
+
+      await openai.beta.threads.messages.create(threadId, message);
+
+      const run = await openai.beta.threads.runs.create(threadId, {
+        assistant_id: process.env.OPENAI_ASSISTANT_ID,
+        stream: true,
       });
-      threadId = thread.id;
-    }
 
-    await openai.beta.threads.messages.create(threadId, message);
+      const metadata = {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "text/plain",
+          "Thread-Id": threadId,
+        },
+      };
 
-    const metadata = {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "text/plain",
-        "Thread-Id": threadId,
-      },
-    };
+      const stream = awslambda.HttpResponseStream.from(
+        responseStream,
+        metadata
+      );
 
-    const stream = awslambda.HttpResponseStream.from(responseStream, metadata);
-
-    const run = await openai.beta.threads.runs.create(threadId, {
-      assistant_id: process.env.OPENAI_ASSISTANT_ID,
-      stream: true,
-    });
-
-    for await (const runObject of run) {
-      switch (runObject.event) {
-        case "thread.message.delta":
-          let chunk = runObject.data;
-          let content = chunk.delta.content;
-          stream.write(content.map((c) => c.text && c.text?.value).join(""));
-          stream.write(bigInvalidBytes);
-          break;
-        case "thread.message.completed":
-          stream.end();
-          break;
-        case "thread.run.failed":
-        case "thread.run.cancelling":
-        case "thread.run.cancelled":
-        case "thread.run.expired":
-        case "thread.run.incomplete":
-        case "thread.message.incomplete":
-          console.log("INTERNAL ERROR: Thread failed", runObject.data);
-          stream.end();
-          break;
-        default:
-          break;
+      for await (const runObject of run) {
+        switch (runObject.event) {
+          case "thread.message.delta":
+            let chunk = runObject.data;
+            let content = chunk.delta.content;
+            stream.write(content.map((c) => c.text && c.text?.value).join(""));
+            stream.write(bigInvalidBytes);
+            break;
+          case "thread.message.completed":
+            stream.end();
+            break;
+          case "thread.run.failed":
+          case "thread.run.cancelling":
+          case "thread.run.cancelled":
+          case "thread.run.expired":
+          case "thread.run.incomplete":
+          case "thread.message.incomplete":
+            console.error("INTERNAL ERROR: Thread failed", runObject.data);
+            stream.end();
+            break;
+          default:
+            break;
+        }
+      }
+    } catch (error) {
+      console.error("INTERNAL ERROR: ", error);
+      const metadata = {
+        statusCode: 500,
+        headers: {
+          "Content-Type": "text/plain",
+        },
+      };
+      const errorMessage =
+        "Oh no--it seems I'm having difficulties connecting. Please try again later!";
+      try {
+        const stream = awslambda.HttpResponseStream.from(
+          responseStream,
+          metadata
+        );
+        stream.end(errorMessage);
+      } catch (ignore) {
+        // This is a fallback if the headers have already been sent.
+        if (!responseStream.writableEnded) responseStream.end(errorMessage);
+        return;
       }
     }
   }
